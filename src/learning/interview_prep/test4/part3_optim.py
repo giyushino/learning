8beb8b41a3d9d28054bb3023d8b62d36640bd997
giyushino <allanzhang440@gmail.com>
2026-07-21 20:14:17 -0700

    adamw

--- src/learning/interview_prep/test4/part3_optim.py
+++ src/learning/interview_prep/test4/part3_optim.py
@@ -43,14 +43,43 @@ class AdamW:
         self.betas = betas
         self.eps = eps
         self.weight_decay = weight_decay
-        # TODO: any state you need (m, v, step count)
+        self.step_count = 0
+        self.states = {}
 
     def zero_grad(self):
-        raise NotImplementedError
+        for param in self.params:
+            param.grad = None
 
+    @torch.no_grad()
     def step(self):
-        raise NotImplementedError
+        self.step_count += 1
+        beta1, beta2 = self.betas
+        for param in self.params:
+            if param.grad is None:
+                continue
+            # weight decay
+            param.mul_(1 - self.lr * self.weight_decay)
+            if param not in self.states:
+                self.states[param] = {
+                    "exp_avg": torch.zeros_like(param),
+                    "exp_avg_sq": torch.zeros_like(param),
+                }
+            states = self.states[param]
+            grad = param.grad
 
+            m, v = states["exp_avg"], states["exp_avg_sq"]
+            m.mul_(beta1).add_(grad, alpha=1 - beta1)
+            v.mul_(beta2).addcmul_(grad, grad, value=1 - beta2)
+
+            m_corr = 1 - beta1 ** self.step_count
+            v_corr = 1 - beta2 ** self.step_count
+            step_size = self.lr / m_corr
+
+            denominator = (v.sqrt() / math.sqrt(v_corr)).add_(self.eps)
+            param.addcdiv_(m, denominator, value=-step_size)
+
+        return
+    
 
 def get_lr(step, max_lr, min_lr, warmup_steps, total_steps):
     """LR schedule used by essentially every modern pretraining run.
@@ -62,7 +91,15 @@ def get_lr(step, max_lr, min_lr, warmup_steps, total_steps):
                  lr = min_lr + 0.5 * (max_lr - min_lr) * (1 + cos(pi * progress))
     - After:   for step > total_steps:  lr = min_lr
     """
-    raise NotImplementedError
+    if step < warmup_steps:
+        lr = max_lr * (step + 1) / warmup_steps
+    elif warmup_steps <= step and step <= total_steps:
+        progress = (step - warmup_steps) / (total_steps - warmup_steps)
+        lr = min_lr + 0.5 * (max_lr - min_lr) * (1 + math.cos(math.pi * progress))
+    else:
+        lr = min_lr
+
+    return lr
 
 
 def accumulated_grads(model, x, y, loss_fn, micro_batch_size):
@@ -80,4 +117,19 @@ def accumulated_grads(model, x, y, loss_fn, micro_batch_size):
 
     Hint: mean-of-means is not the mean when the chunks aren't equal sized.
     """
-    raise NotImplementedError
+    batch_size = x.size(0)
+    model.zero_grad(set_to_none=True)
+    loss = 0.0
+    for start in range(0, batch_size, micro_batch_size):
+        end = start + micro_batch_size
+        x_chunk = x[start:end]
+        y_chunk = y[start:end]
+        mini_batch_size = x_chunk.size(0)
+        chunked_loss = loss_fn(model(x_chunk), y_chunk)
+        weighted = (chunked_loss * mini_batch_size) / batch_size
+        weighted.backward()
+        loss += weighted.item()
+
+    return loss
+
+        
