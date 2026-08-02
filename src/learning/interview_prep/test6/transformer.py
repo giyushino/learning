71c4655ebee024be363c02d6524b766214648a1c
giyushino <allanzhang440@gmail.com>
2026-08-01 20:33:56 -0700

    init transformer implementation

---
+++ src/learning/interview_prep/test6/transformer.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+
+if __name__ == "__main__":
+    block = TransformerBlock()
